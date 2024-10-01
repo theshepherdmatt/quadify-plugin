c9af64137626c507270af41505bd049cd1c5a771
Quadify <matt.theshepherd@gmail.com>
2024-10-01 19:47:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,196 +1,202 @@
 'use strict';
-var libQ = require('kew');
-var libNet = require('net');
-var fs = require('fs-extra');
-var config = new (require('v-conf'))();
-var exec = require('child_process').exec;
-var Gpio = require('onoff').Gpio;
 
-module.exports = ControllerQuadify;
+const libQ = require('kew'),
+      { exec } = require('child_process'),
+      http = require('http');
 
-function ControllerQuadify(context) {
-    var self = this;
+module.exports = QuadifyPlugin;
 
+function QuadifyPlugin(context) {
     this.context = context;
     this.commandRouter = this.context.coreCommand;
     this.logger = this.context.logger;
     this.configManager = this.context.configManager;
 }
 
-ControllerQuadify.prototype.onVolumioStart = function() {
-    var self = this;
-    self.logger.info("Quadify plugin initiated");
+QuadifyPlugin.prototype.getI18nFile = function (langCode) {
+    const dir = __dirname + '/i18n/',
+        files = fs.readdirSync(dir),
+        targetFile = 'strings_' + langCode + '.json';
+    if (files.some(x => x === targetFile)) return dir + targetFile;
+    return dir + 'strings_en.json';
+};
 
-    this.configFile = this.commandRouter.pluginManager.getConfigurationFile(this.context, 'config.json');
-    self.getConf(this.configFile);
-
+QuadifyPlugin.prototype.onVolumioStart = function() {
+    const configFile = this.commandRouter.pluginManager.getConfigurationFile(this.context, 'config.json');
+    this.config = new (require('v-conf'))();
+    this.config.loadFile(configFile);
     return libQ.resolve();
 };
 
-ControllerQuadify.prototype.onVolumioReboot = function() {
-    var self = this;
-    self.softShutdown.writeSync(1);
-};
+QuadifyPlugin.prototype.onStart = function() {
+    const defer = libQ.defer();
+    this.logger.info("Quadify: Starting Plugin");
 
-ControllerQuadify.prototype.onVolumioShutdown = function() {
-    var self = this;
-    var defer = libQ.defer();
+    this.commandRouter.loadI18nStrings();
 
-    self.softShutdown.writeSync(1);
-    setTimeout(function() {
-        self.softShutdown.writeSync(0);
-        defer.resolve();
-    }, 1000);
-
-    return defer;
-};
-
-ControllerQuadify.prototype.getConfigurationFiles = function() {
-    return ['config.json'];
-};
-
-// Plugin methods
-ControllerQuadify.prototype.onStop = function() {
-    var self = this;
-    self.logger.info("Performing Quadify onStop action");
-
-    if (self.bootOk != undefined) self.bootOk.unexport();
-    if (self.softShutdown != undefined) self.softShutdown.unexport();
-    if (self.shutdownButton != undefined) {
-        self.shutdownButton.unwatchAll();
-        self.shutdownButton.unexport();
-    }
-
-    return libQ.resolve();
-};
-
-ControllerQuadify.prototype.stop = function() {
-    var self = this;
-    self.logger.info("Performing Quadify stop action");
-
-    return libQ.resolve();
-};
-
-ControllerQuadify.prototype.onStart = function() {
-    var self = this;
-    self.logger.info("Configuring Quadify GPIO pins");
-
-    // Soft shutdown GPIO configuration
-    if (self.tryParse(self.config.get('soft_shutdown'), 0) != 0) {
-        self.softShutdown = new Gpio(parseInt(self.config.get('soft_shutdown')), 'out');
-        self.logger.info('Quadify: Soft shutdown GPIO binding... OK');
-    }
-
-    // Shutdown button GPIO configuration
-    if (self.tryParse(self.config.get('shutdown_button'), 0) != 0) {
-        self.shutdownButton = new Gpio(parseInt(self.config.get('shutdown_button')), 'in', 'both');
-        self.logger.info('Quadify: Hardware button GPIO binding... OK');
-    }
-
-    // Boot OK GPIO configuration
-    if (self.tryParse(self.config.get('boot_ok'), 0) != 0) {
-        self.bootOk = new Gpio(parseInt(self.config.get('boot_ok')), 'high');
-        self.logger.info('Quadify: Boot OK GPIO binding... OK');
-    }
-
-    // Watch for hardware button press
-    if (self.shutdownButton) {
-        self.shutdownButton.watch(self.hardShutdownRequest.bind(this));
-    }
-
-    return libQ.resolve();
-};
-
-ControllerQuadify.prototype.onRestart = function() {
-    var self = this;
-    self.logger.info("Performing Quadify onRestart action");
-};
-
-ControllerQuadify.prototype.onInstall = function() {
-    var self = this;
-    self.logger.info("Performing Quadify onInstall action");
-};
-
-ControllerQuadify.prototype.onUninstall = function() {
-    var self = this;
-    self.logger.info("Performing Quadify onUninstall action");
-};
-
-ControllerQuadify.prototype.getUIConfig = function() {
-    var self = this;
-    var defer = libQ.defer();
-    var lang_code = this.commandRouter.sharedVars.get('language_code');
-    self.getConf(this.configFile);
-    self.logger.info("Quadify: Loaded previous config.");
-
-    self.commandRouter.i18nJson(__dirname + '/i18n/strings_' + lang_code + '.json',
-        __dirname + '/i18n/strings_en.json',
-        __dirname + '/UIConfig.json')
-        .then(function(uiconf) {
-            self.logger.info("Quadify: Populating UI...");
-
-            // GPIO configuration
-            uiconf.sections[0].content[0].value = self.config.get('soft_shutdown');
-            uiconf.sections[0].content[1].value = self.config.get('shutdown_button');
-            uiconf.sections[0].content[2].value = self.config.get('boot_ok');
-
-            self.logger.info("Quadify: UI configuration loaded");
-
-            defer.resolve(uiconf);
-        })
-        .fail(function() {
-            defer.reject(new Error());
-        });
-
+    // Create symbolic links for OLED scripts to access the configuration file
+    this.configSoftLinks([`${__dirname}/apps/oled`])
+        .then(() => this.systemctl('daemon-reload')) // Reload the systemd configuration
+        .then(() => this.startServiceIfActive("oled_active", "oled")) // Start the OLED service if enabled
+        .then(() => this.setRemoteActive(this.config.get("remote_active"))) // Handle remote configuration if applicable
+        .then(() => defer.resolve())
+        .fail(err => this.logger.error("Quadify Error: " + err));
     return defer.promise;
 };
 
-ControllerQuadify.prototype.setUIConfig = function(data) {
-    var self = this;
-    self.logger.info("Quadify: Updating UI config");
-    return libQ.resolve();
+QuadifyPlugin.prototype.onStop = function() {
+    const defer = libQ.defer();
+    this.systemctl('stop oled.service')
+        .then(() => this.systemctl('stop lircd.service'))
+        .then(() => this.systemctl('stop irexec.service'))
+        .then(() => defer.resolve())
+        .fail(err => defer.reject(err));
+    return defer.promise;
 };
 
-ControllerQuadify.prototype.getConf = function(configFile) {
-    var self = this;
-    this.config = new(require('v-conf'))();
-    this.config.loadFile(configFile);
-
-    return libQ.resolve();
+QuadifyPlugin.prototype.onRestart = function() {
+    const defer = libQ.defer();
+    this.commandRouter.loadI18nStrings();
+    this.systemctl('restart oled.service')
+        .then(() => this.systemctl('restart lircd.service'))
+        .then(() => this.systemctl('restart irexec.service'))
+        .then(() => defer.resolve())
+        .fail(err => defer.reject());
+    return defer.promise;
 };
 
-ControllerQuadify.prototype.setConf = function(conf) {
-    return libQ.resolve();
+QuadifyPlugin.prototype.restartOled = function() {
+    const defer = libQ.defer();
+    this.systemctl('restart oled.service')
+        .then(() => defer.resolve())
+        .fail(err => defer.reject());
+    return defer.promise;
 };
 
-// Public Methods
-ControllerQuadify.prototype.updateButtonConfig = function(data) {
-    var self = this;
-
-    self.config.set('soft_shutdown', data['soft_shutdown']);
-    self.config.set('shutdown_button', data['shutdown_button']);
-    self.config.set('boot_ok', data['boot_ok']);
-
-    self.commandRouter.pushToastMessage('success', 'Quadify: Successfully saved new configuration.');
-    return libQ.resolve();
-};
-
-// Button Management
-ControllerQuadify.prototype.hardShutdownRequest = function(err, value) {
-    var self = this;
-    if (value == 1) { // Only proceed if the button press is detected (value = 1)
-        self.commandRouter.shutdown();
+// Configuration Methods
+QuadifyPlugin.prototype.startServiceIfActive = function(config, service) {
+    const defer = libQ.defer();
+    if (this.config.get(config)) {
+        this.systemctl(`restart ${service}.service`).then(() => defer.resolve()).fail(err => defer.reject(err));
+    } else {
+        return libQ.resolve();
     }
+    return defer.promise;
 };
 
-ControllerQuadify.prototype.tryParse = function(str, defaultValue) {
-    var retValue = defaultValue;
-    if (str !== null) {
-        if (str.length > 0) {
-            if (!isNaN(str)) {
-                retValue = parseInt(str);
-            }
+QuadifyPlugin.prototype.getUIConfig = function() {
+    const defer = libQ.defer(),
+          lang_code = this.commandRouter.sharedVars.get('language_code'),
+          target_lang_path = `${__dirname}/i18n/strings_${lang_code}.json`,
+          fallback_lang_path = `${__dirname}/i18n/strings_en.json`,
+          config_template_path = `${__dirname}/UIConfig.json`;
+
+    this.commandRouter.i18nJson(target_lang_path, fallback_lang_path, config_template_path)
+        .then(uiconf => {
+            uiconf.sections[1].content[0].value = this.config.get('oled_active');
+            uiconf.sections[1].content[1].value = parseInt(this.config.get('contrast'));
+            uiconf.sections[1].content[1].attributes = [{ min: 1, max: 254 }];
+            uiconf.sections[1].content[2].value = parseInt(this.config.get('sleep_after'));
+            uiconf.sections[1].content[2].attributes = [{ min: 1 }];
+            uiconf.sections[1].content[3].value = parseInt(this.config.get('deep_sleep_after'));
+            uiconf.sections[1].content[3].attributes = [{ min: 1 }];
+
+            uiconf.sections[2].content[0].value = this.config.get('remote_active');
+            defer.resolve(uiconf);
+        })
+        .fail(err => defer.reject());
+    return defer.promise;
+};
+
+QuadifyPlugin.prototype.getConfigurationFiles = function() {
+    return ['config.json'];
+};
+
+QuadifyPlugin.prototype.updateOledConfig = function(data) {
+    const defer = libQ.defer();
+    this.config_changes = {};
+    this.config_errors = [];
+
+    this.validateAndUpdateConfigItem(data, "oled_active");
+    this.validateAndUpdateConfigItem(data, "contrast", x => x > 0 && x < 255);
+    this.validateAndUpdateConfigItem(data, "sleep_after", x => x >= 0);
+    this.validateAndUpdateConfigItem(data, "deep_sleep_after", x => x >= 0);
+
+    if (!Object.keys(this.config_changes).length) {
+        this.commandRouter.pushToastMessage('info', "Quadify: ", this.commandRouter.getI18nString('UI.CONFIG_NOCHANGE'));
+    } else {
+        this.commandRouter.pushToastMessage('success', "Quadify: ", this.commandRouter.getI18nString('UI.CONFIG_UPDATE'));
+    }
+
+    let returnValue = null;
+    if ("oled_active" in this.config_changes) {
+        returnValue = defer;
+        if (this.config.get("oled_active")) {
+            this.systemctl("restart oled.service").then(() => defer.resolve()).fail(err => defer.reject());
+        } else {
+            this.systemctl("stop oled.service").then(() => defer.resolve()).fail(err => defer.reject());
+        }
+        delete this.config_changes["lcd_active"];
+    } else returnValue = defer.resolve();
+
+    for (let err of this.config_errors) this.commandRouter.pushToastMessage('error', "Quadify: ", err);
+
+    for (let key in this.config_changes) {
+        if (key in ["contrast", "sleep_after", "deep_sleep_after"]) {
+            try {
+                http.get(`http://127.0.0.1:4153/${key}=${this.config_changes[key]}`);
+            } catch (e) {}
         }
     }
-    return retValue;
+
+    this.logger.info('Quadify: OLED configuration updated from UI.');
+    return returnValue;
+};
+
+QuadifyPlugin.prototype.validateAndUpdateConfigItem = function(obj, key, validation_rule) {
+    if (obj && key && obj[key] !== undefined && obj[key] != this.config.get(key)) {
+        if (!validation_rule || validation_rule(obj[key])) {
+            this.config.set(key, obj[key]);
+            this.config_changes[key] = obj[key];
+        } else {
+            this.config_errors.push(`Quadify: Invalid config value ${key} ${obj[key]}. `);
+        }
+    }
+};
+
+QuadifyPlugin.prototype.configSoftLinks = function(targets) {
+    if (!targets || !targets.length) return libQ.resolve();
+
+    const defer = libQ.defer(),
+          todo = [];
+
+    for (let target of targets) {
+        todo.push(
+            new Promise((resolve, reject) => {
+                exec(`/bin/ln -s -f ${this.config.filePath} ${target}`, { uid: 1000, gid: 1000 }, (err) => {
+                    err && reject(err) || resolve();
+                });
+            })
+        );
+    }
+
+    Promise.all(todo).then(() => defer.resolve()).catch(err => defer.reject());
+    return defer.promise;
+};
+
+QuadifyPlugin.prototype.systemctl = function(cmd) {
+    const defer = libQ.defer(),
+          handle = (err, stdout, stderr) => {
+              if (err) {
+                  this.logger.error(`Quadify: systemd failed cmd ${cmd} : ${err}`);
+                  this.commandRouter.pushToastMessage('error', "Quadify:", `Systemd failed cmd: ${cmd} : ${err}.`);
+                  defer.reject();
+                  return;
+              }
+              this.logger.info(`Quadify: systemd cmd ${cmd} : success`);
+              defer.resolve();
+          };
+
+    exec('/usr/bin/sudo /bin/systemctl ' + cmd, { uid: 1000, gid: 1000 }, handle);
+    return defer.promise;
 };
